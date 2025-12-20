25b0ed2c4a9a0f4b704e85c1104751e153f8c8e0
ikegami <ikegamitky@gmail.com>
2025-12-20 20:19:28 +0900

    Fix package runtime paths and app outputs

--- app.R
+++ app.R
@@ -1,17 +1,7 @@
-# Sys.setenv(FELIS_DATA_ROOT = "/srv/shiny-server/felis-ccat")
-# Sys.setenv(FELIS_DATA_ROOT = getwd())
-# FELIS::run_app()
-
-
-#Sys.setenv(FELIS_DATA_ROOT = "/srv/shiny-server/felis-cs")
-Sys.setenv(FELIS_DATA_ROOT = "/Users/ikegami/Desktop/length_bias/felis-ccat")
+# Sys.setenv(FELIS_DATA_ROOT = "/srv/shiny-server/felis-cs")
+Sys.setenv(FELIS_DATA_ROOT = getwd())
 APP_DIR <- system.file("app", package = "FELIS")
-stopifnot(nzchar(APP_DIR))
-
-old <- getwd()
-on.exit(setwd(old), add = TRUE)
 setwd(APP_DIR)
-
 source("app.R", local = TRUE, chdir = TRUE)
 # Run the application
 shinyApp(ui = ui, server = server)
