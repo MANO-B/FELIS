12065f0ae7a1fcb9224ae712202e8375368650fa
ikegami <ikegamitky@gmail.com>
2025-12-20 17:53:58 +0900

    Fix package runtime paths and app outputs

--- R/run_app.R
+++ R/run_app.R
@@ -3,5 +3,5 @@
 
 run_app <- function(data_root = NULL, ...) {
   app_dir <- system.file("app", package = "FELIS")
-  shiny::runApp(app_dir, ...)
+  shiny::runApp(app_dir, launch.browser = F, ...)
 }
--- app.R
+++ app.R
@@ -1,1 +1,3 @@
+# Sys.setenv(FELIS_DATA_ROOT = "/srv/shiny-server/felis-ccat")
+# Sys.setenv(FELIS_DATA_ROOT = getwd())
 FELIS::run_app()
